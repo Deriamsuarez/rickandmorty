6de27b8fab4c3445b6ba6f7e7f5bc0076384f524
Deriamsuarez <45581669+Deriamsuarez@users.noreply.github.com>
2022-08-02 17:10:48 -0400

    f

--- src/components/CardsBox.jsx
+++ src/components/CardsBox.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import Card from './Card'
+import Card from './Card.jsx'
 
 const CardsBox = ({ planet }) => {
   const [residentsUtilArray, setResidentsUtilArray] = useState()
